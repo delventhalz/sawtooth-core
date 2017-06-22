ae59478ace1c7097c9b9205ce6ec6b39a96eb103
Zac Delventhal <delventhalz@gmail.com>
2017-06-22 11:38:26 -0500

    WIP

--- validator/sawtooth_validator/state/batch_tracker.py
+++ validator/sawtooth_validator/state/batch_tracker.py
@@ -17,15 +17,18 @@ import abc
 from threading import RLock
 from sawtooth_validator.journal.timed_cache import TimedCache
 from sawtooth_validator.journal.block_store import StoreUpdateObserver
+from sawtooth_validator.execution.executor import InvalidTransactionObserver
 from sawtooth_validator.journal.journal import PendingBatchObserver
 from sawtooth_validator.protobuf.client_pb2 import BatchStatus
 
 
-# By default pending batch ids will be kept for one hour
-PENDING_KEEP_TIME = 3600
+# By default invalid batch info will be kept for one hour
+INVALID_KEEP_TIME = 3600
 
 
-class BatchTracker(StoreUpdateObserver, PendingBatchObserver):
+class BatchTracker(StoreUpdateObserver,
+                   InvalidTransactionObserver,
+                   PendingBatchObserver):
     """Tracks batch statuses for this local validator, allowing interested
     components to check where a batch is in the validation process. It should
     only be relied on for batches submitted locally, and is not persisted
@@ -40,29 +43,56 @@ class BatchTracker(StoreUpdateObserver, PendingBatchObserver):
     """
     def __init__(self, block_store):
         self._block_store = block_store
-        self._pending = TimedCache(keep_time=PENDING_KEEP_TIME)
+        self._invalid = TimedCache(keep_time=INVALID_KEEP_TIME)
+        self._pending = {}
 
         self._lock = RLock()
         self._observers = {}
 
     def notify_store_updated(self):
-        """Removes batches from the pending cache if found in the block store,
+        """Removes batches from the pending set if found in the block store,
         and notifies any observers.
         """
         with self._lock:
-            for batch_id in set(self._pending):
+            for batch_id in self._pending.copy():
                 if self._block_store.has_batch(batch_id):
                     self._pending.pop(batch_id)
                     self._update_observers(batch_id, BatchStatus.COMMITTED)
 
-    def notify_batch_pending(self, batch_id):
-        """Adds a batch to the pending list.
+    def notify_txn_invalid(self, txn_id, message=None, extended_data=None):
+        """Adds a batch id to the invalid cache along with the id of the
+        transaction that was rejected and any error message or extended data.
+        Removes that batch id from the pending set. The cache is only
+        temporary, and the batch info will be purged after one hour.
 
         Args:
-            batch_id (str): The id of the pending batch
+            txn_id (str): The id of the invalid batch
+            message (str, optional): Message explaining why batch is invalid
+            extended_data (bytes, optional): Additional error data
         """
+        txn_info = {'id': txn_id}
+        if message is not None:
+            txn_info['message'] = message
+        if extended_data is not None:
+            txn_info['extended_data'] = extended_data
+
         with self._lock:
-            self._pending[batch_id] = True
+            for batch_id, txn_ids in self._pending.copy().items():
+                if txn_id in txn_ids:
+                    self._invalid[batch_id] = txn_info
+                    self._pending.pop(batch_id)
+                    self._update_observers(batch_id, BatchStatus.INVALID)
+                    return
+
+    def notify_batch_pending(self, batch):
+        """Adds a Batch id to the pending cache, with its transaction ids.
+
+        Args:
+            batch (str): The id of the pending batch
+        """
+        txn_ids = {t.header_signature for t in batch.transactions}
+        with self._lock:
+            self._pending[batch.header_signature] = txn_ids
             self._update_observers(batch_id, BatchStatus.PENDING)
 
     def get_status(self, batch_id):
@@ -77,6 +107,8 @@ class BatchTracker(StoreUpdateObserver, PendingBatchObserver):
         with self._lock:
             if self._block_store.has_batch(batch_id):
                 return BatchStatus.COMMITTED
+            if batch_id in self._invalid:
+                return BatchStatus.INVALID
             if batch_id in self._pending:
                 return BatchStatus.PENDING
             return BatchStatus.UNKNOWN
@@ -92,6 +124,28 @@ class BatchTracker(StoreUpdateObserver, PendingBatchObserver):
         """
         with self._lock:
             return {b: self.get_status(b) for b in batch_ids}
+
+    def get_invalid_txn_info(self, batch_id):
+        """Fetches the id of the Transaction that failed within a particular
+        Batch, as well as any error message or other data about the failure.
+
+        Args:
+            batch_id (str): The id of the Batch containing an invalid txn
+
+        Raises:
+            KeyError: Raised if the specified batch is not invalid
+
+        Returns:
+            dict: A dict with three possible keys:
+                * 'id' - the header_signature of the invalid Transaction
+                * 'message' - the error message sent by the TP
+                * 'extended_data' - any additional data sent by the TP
+        """
+        try:
+            return self._invalid[batch_id]
+        except KeyError:
+            raise KeyError(
+                'Specified batch id not in invalid cache: {}'.format(batch_id))
 
     def watch_statuses(self, observer, batch_ids):
         """Allows a component to register to be notified when a set of
